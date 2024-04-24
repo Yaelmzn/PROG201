c3e724568ae86244385452db54cc7b13e9ed9b09
Yael Mazin <yael.mazin@gmail.com>
2024-04-24 11:05:24 +0200

    Defining functions for creating stimuli and restricting the lines into the circle

--- target_detection.py
+++ target_detection.py
@@ -30,6 +30,7 @@ LINE_WIDTH = 2
 OBLIQUE_ANGLE = 20
 WIDTH_SCREEN = 1080
 LENGTH_SCREEN = 1920
+CIRCLE_RADIUS = 360
 
 exp = design.Experiment(name="Target Detection", text_size=40, background_colour=WHITE)
 #control.set_develop_mode(on=True)
@@ -52,6 +53,49 @@ instructions = stimuli.TextScreen("Instructions",
 
 exp.add_data_variable_names(['trial', 'key', 'rt', 'nb_distractors'])
 
+def choose_start_point():
+    x_coordinate = random.randrange(-CIRCLE_RADIUS+10,CIRCLE_RADIUS-LINE_LENGTH-10)
+    angle=math.acos(x_coordinate/CIRCLE_RADIUS)
+    y_coordinate = random.randrange(round((-CIRCLE_RADIUS+10)*math.sin(angle)),round((CIRCLE_RADIUS-LINE_LENGTH-10)*math.sin(angle)))
+
+    return((x_coordinate,y_coordinate))
+
+
+def make_stimulus(target_presence, target_type, nb_distractors, circle): 
+    if target_presence == False : 
+        if target_type == "oblique target":
+            for i in range (nb_distractors+2):
+                start_point = np.array(choose_start_point())
+                vertical = stimuli.Line(start_point, (start_point+vertical_relative_end_point), line_width=LINE_WIDTH, colour=BLACK)
+                vertical.plot(circle)
+
+        elif target_type == "vertical target":
+            for i in range (nb_distractors+2):
+                start_point_oblique = np.array(choose_start_point())
+                oblique = stimuli.Line(start_point_oblique, (start_point_oblique+oblique_relative_end_point), line_width=LINE_WIDTH, colour=BLACK)
+                oblique.plot(circle)
+
+    elif target_presence == True:
+        if target_type == "oblique target":
+            for i in range (nb_distractors+1):
+                start_point = np.array(choose_start_point())
+                vertical = stimuli.Line(start_point, (start_point+vertical_relative_end_point), line_width=LINE_WIDTH, colour=BLACK)
+                vertical.plot(circle)
+            start_point_oblique = np.array(choose_start_point())
+            oblique = stimuli.Line(start_point_oblique, start_point_oblique+oblique_relative_end_point, line_width=LINE_WIDTH, colour=BLACK)
+            oblique.plot(circle)
+
+        elif target_type == "vertical target":
+            for i in range (nb_distractors+1):
+                start_point_oblique = np.array(choose_start_point())
+                oblique = stimuli.Line(start_point_oblique, (start_point_oblique+oblique_relative_end_point), line_width=LINE_WIDTH, colour=BLACK)
+                oblique.plot(circle)
+            start_point = np.array(choose_start_point())
+            vertical = stimuli.Line(start_point, start_point+vertical_relative_end_point, line_width=LINE_WIDTH, colour=BLACK)
+            vertical.plot(circle)
+    return(circle)
+
+
 control.start(skip_ready_screen=True)
 instructions.present()
 exp.keyboard.wait()
@@ -59,32 +103,21 @@ exp.keyboard.wait()
 # Define possible positions :
 # circle = stimuli.Circle(360, colour=BLACK, line_width=2)
 
-conditions = ["oblique target", "vertical target"]
-trials=conditions*int((N_TRIALS/len(conditions)))
-random.shuffle(trials)
+target_type = ["oblique target", "vertical target"]
+target_presences = [True, False] 
+list_conditions = []
+for target in target_type :
+    for presence in target_presences:
+        list_conditions.append({"target_type":target,"target_presence": presence})
+list_trials=list_conditions*int((N_TRIALS/len(list_conditions)))
+random.shuffle(list_trials)
 
 nb_distractors = 20
 
-for trial in trials:
+for trial in list_trials:
     circle = stimuli.Circle(360, colour=BLACK, line_width=2)
-    if trial == "oblique target":
-        for j in range (nb_distractors+1):
-            start_point = np.array((random.randrange(-360,360),random.randrange(-360,360)))
-            vertical = stimuli.Line(start_point, (start_point+vertical_relative_end_point), line_width=LINE_WIDTH, colour=BLACK)
-            vertical.plot(circle)
-        start_point_oblique = np.array((random.randrange(-360,360),random.randrange(-360,360)))
-        oblique = stimuli.Line(start_point_oblique, start_point_oblique+oblique_relative_end_point, line_width=LINE_WIDTH, colour=BLACK)
-        oblique.plot(circle)
-
-    if trial == "vertical target":
-        for j in range (nb_distractors+1):
-            start_point_oblique = np.array((random.randrange(-360,360),random.randrange(-360,360)))
-            oblique = stimuli.Line(start_point_oblique, (start_point_oblique+oblique_relative_end_point), line_width=LINE_WIDTH, colour=BLACK)
-            oblique.plot(circle)
-        start_point = np.array((random.randrange(-360,360),random.randrange(-360,360)))
-        vertical = stimuli.Line(start_point, start_point+vertical_relative_end_point, line_width=LINE_WIDTH, colour=BLACK)
-        vertical.plot(circle)
-    circle.present()
+    stimulus=make_stimulus(trial["target_presence"], trial["target_type"], nb_distractors, circle)  
+    stimulus.present()
     key, rt = exp.keyboard.wait()
     exp.data.add([trial, key, rt, nb_distractors])
     blankscreen.present()
